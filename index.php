1b26820f8c6f8b28696bd4e01f45e923b9b92c12
Kevin Olson <kolson@si5229.si.net>
2017-10-10 11:38:46 -0400

    schedule added

--- index.php
+++ index.php
@@ -6,31 +6,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Sprinting Turtles</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+
     <link rel="stylesheet" type="text/css" href="css/styles.css">
     <link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.css">
     <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
   </head>
 
   <body>
-   <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+ 
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="brand" href="#">Project name</a>
-          <div class="nav-collapse collapse">
-            <ul class="nav">
-              <li class="active"><a href="#">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
+          
         </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+
+            <li><img src="images/logo2.jpg"></li>
+            <li><a href="#">The Sprinting Turtles</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#about">About</a></li>
+            <li><a href="#ourStuff">Music</a></li>
+            <li><a target="_blank" href="https://www.instagram.com/thesprintingturtles/"><i class="fa fa-lg fa-instagram"></i></a></li>
+            <li><a target="_blank" href="https://www.facebook.com/thesprintingturtles" ><i class="fa fa-lg fa-facebook"></i></a></li>
+            
+          </ul>
+        </div><!--/.nav-collapse -->
       </div>
-    </div>
+    </nav>
 
    <!--   HERO SECTION -->
      <div class="hero">
@@ -43,13 +54,13 @@
        <div class="wrapper row">
          <div class="medium-12 column">
           <h1>ABOUT</h1>
-          <p>We are The Sprinting Turtles.  Rock band from Washington DC.  Check out our music and who we are!</p>
+          <p>We are The Sprinting Turtles <br /> Rock band from Washington DC.  Check out our music and who we are!</p>
           <br />
             <div class="row">
               <div class="col-sm-3 bandMember">
-                <h3>Kevin Olson</h3>
+                <a href="http://www.kevinjolson.com"><h3>Kevin Olson</h3></a>
                 <h4>Lead Guitar & Vocals</h4>
-                <img src="images/kevin.jpg">
+                <a href="http://www.kevinjolson.com"><img src="images/kevin.jpg"></a>
               </div>
               <div class="col-sm-3 bandMember">
                 <h3>Justin Johnson</h3>
@@ -58,7 +69,7 @@
               </div>
               <div class="col-sm-3 bandMember">
                 <h3>Kevin Ralph</h3>
-                <h4>Drums</h4>
+                <h4>Drums and Bag Pipes</h4>
                 <img src="images/kevinr.jpg">
               </div>
               <div class="col-sm-3 bandMember">
@@ -68,7 +79,16 @@
               </div>
 
             </div>
-            
+            <br />
+            <br />
+            <br />
+            <br />
+            <h1>Schedule</h1>
+          <p>October 27 : Private Event
+          <br />
+            September 3, 2018 : Private Event
+          </p>
+
          </div>
        </div>
      </section>
@@ -85,12 +105,12 @@
               <iframe width="460" height="215" src="https://www.youtube.com/embed/PwZwMHHgUcg" frameborder="0" allowfullscreen></iframe>
               <iframe width="460" height="215" src="https://www.youtube.com/embed/HVIAeI4O8jw" frameborder="0" allowfullscreen></iframe>
             </div>
-            <div class="col-lg-6" id="gallery" style="border: solid red;">
+            <div class="col-lg-6" id="gallery">
               <h3>Photos</h3>
             <div class="ImageContainer">
               <div class="lightbox-gallery">
                 <?php  
-                  $dirname = "images/";
+                  $dirname = "images/gallery/";
                   $images = glob($dirname."*.jpg");
                   foreach ($images as $image) {
                     echo '<div><img src="'.$image.'" /></div>';
@@ -103,17 +123,15 @@
            
          </div>
        </div>
+
      </section>
-
 <!-- FOOTER -->
-  <footer>
-  <h1>The Sprinting Turtles</h1>
-    <div class="container text-center">
 
-    <!--   <a target="_blank" href="https://www.instagram.com/ma_addix/"><i class="fa fa-2x fa-instagram"></i></a> &nbsp
-      <a target="_blank" href="https://www.youtube.com/channel/UCEjYZO3Mw_voTiDboWBl7Sg"><i class="fa fa-2x fa-youtube"></i></a> -->
-    </div>
+  <footer>
+    <h1>The Sprinting Turtles</h1>
   </footer>
+
+
 
     <script src="js/vendor/jquery.js"></script>
     <script src="js/bootstrap.js"></script>
