8fcaf477ed2a1ed41a5ba17087f7b49009892922
Kevin Olson <kolson@Administrators-MacBook-Pro-2.local>
2018-12-18 15:32:20 -0500

    The New Sprinting Turtles

--- index.php
+++ index.php
@@ -15,34 +15,6 @@
   </head>
 
   <body>
- 
-<!--     <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li><img src="images/logo2.jpg"></li>
-            <li><a href="#">The Sprinting Turtles</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#about">About</a></li>
-            <li><a href="#ourStuff">Music</a></li>
-            <li><a href="#schedule">Schedule</a></li>
-            <li><a target="_blank" href="https://www.instagram.com/thesprintingturtles/"><i class="fa fa-lg fa-instagram"></i></a></li>
-            <li><a target="_blank" href="https://www.facebook.com/thesprintingturtles" ><i class="fa fa-lg fa-facebook"></i></a></li>
-            
-          </ul>
-        </div>
-      </div>
-    </nav> -->
     <div id="nav-trigger" class="nav-trigger open" style="z-index: 999;">
     <span class="line"></span>
     <span class="line"></span>
@@ -68,56 +40,50 @@
        <div class="wrapper row">
          <div class="medium-12 column">
           <h1>ABOUT</h1>
-          <p>We are The Sprinting Turtles! <br /> Rock band from Washington DC formed while Kevin and Justin were studying at Virginia Tech.  We had a blast playing live music at house parties.  After graduating Justin and Kevin moved to the Washington DC area and wanted to continue to play music.  We still bring the same goofy energy to our shows today and it is always a blast getting the crowd involved and singing along with us.  Check out our music and who we are!</p>
+          <p>We are The Sprinting Turtles! <br /> Rock band from Washington DC formed while Kevin and Justin were studying at Virginia Tech.  After graduating Justin and Kevin moved to the Washington DC area and wanted to continue to play music.  We still bring the same goofy energy to our shows today and it is always a blast getting the crowd involved and singing along with us.  Check out our music and who we are!</p>
           <br />
             <div class="row">
-              <div class="col-sm-4 bandMember">
+              <div class="col-sm-6 bandMember">
                 <a href="http://www.kevinjolson.com"><h3>Kevin Olson</h3></a>
                 <h4>Lead Guitar & Vocals</h4>
                 <a href="http://www.kevinjolson.com"><img src="images/kevin.jpg"></a>
               </div>
-              <div class="col-sm-4 bandMember">
+              <div class="col-sm-6 bandMember">
                 <h3>Justin Johnson</h3>
                 <h4>Bass & Vocals</h4>
                 <img src="images/justin.jpg">
               </div>
-              <div class="col-sm-4 bandMember">
+              <!-- <div class="col-sm-4 bandMember">
                 <h3>Kevin Ralph</h3>
                 <h4>Drums and Bag Pipes</h4>
                 <img src="images/kevinr.jpg">
-              </div>
+              </div> -->
 
             </div>
             <br />
             <br />
             <br />
             <br />
-            <h1 id="schedule">Schedule</h1>
+            <h1 id="schedule">2019 Schedule</h1>
+            <p>January 26 - Tucked Away Brewery</p>
+            <p>March 7 - Light Horse</p>
+            <p>December 7 - Paradise Springs</p>
+            
+            <br/>
             <p>
-            September 30 : Boomerang Pirate Ship
-              <br/>
-            November 17 : Alley Cat
-              <br />
-            December 8: Paradise Springs Winery
-              <br />
-              <br />
-            PAST VENUES
-              <br/>
-              Boomerang Pirate Ship
-              <br />
-              Gypsy Sallys
-              <br/>
-              Tysons Biergarten
-              <br/>
-              Auld Shebeen
-              <br/>
-              Arlington County Fair
-              <br/>
-              Jammin Java
-              <br/>
-              Cowboy Cafe
-              <br/>
-              Westover Beer Garden
+
+              PAST VENUES <br/>
+              Alley Cat <br /> 
+              Arlinton County Fair <br />
+              Auld Shebeen <br />
+              Boomerang Pirate Ship <br />
+              Cowboy Cafe <br />
+              Gypsy Sallys <br />
+              Jammin Java <br />
+              Tysons Biergarten <br />
+              Westover Beer Garden <br />
+
+
           </p>
          </div>
        </div>
